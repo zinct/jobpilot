052a4a5f0b559a4ef9a026aaff86bb18a8322796
Indra Mahesa <indramahesa128@gmail.com>
2025-03-28 23:48:52 +0700

    feat: remove all animation & react router due to lack of support on ICP Ninja

--- playground.sh
+++ playground.sh
@@ -3,15 +3,7 @@
 while true; do
     echo "Deploying..."
     dfx deploy --playground 
-
-    echo "Restarting in 15 minutes... (Press 'R' to restart immediately)"
-
-    for ((i = 0; i < 900; i++)); do
-        read -t 1 -n 1 key
-        if [[ $key == "R" || $key == "r" ]]; then
-            echo "Force restart triggered!"
-            break
-        fi
-        sleep 1
-    done
+    
+    echo "Waiting for 15 minutes..."
+    sleep 900 
 done
